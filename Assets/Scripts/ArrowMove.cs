692f0e204332338eaa21bf68dc4a484dec9bc713
girayozel <ozel.giray@gmail.com>
2016-05-29 14:41:30 +0200

    Arrow

---
+++ Assets/Scripts/ArrowMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ArrowMove : MonoBehaviour
+{
+    public float speed = 100;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, Mathf.PingPong(Time.time * speed, 90)));        
+    }
+}
---
+++ Assets/Scripts/UserControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof (Character2D))]
+    public class UserControl : MonoBehaviour
+    {
+        private Character2D character;
+        private bool jump;
+
+        private void Awake()
+        {
+            character = GetComponent<Character2D>();
+        }
+
+        private void Update()
+        {
+            if(!jump)
+            // Read the jump input in Update so button presses aren't missed.
+            jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            character.Move(h, jump);
+            jump = false;
+        }
+    }
+}
